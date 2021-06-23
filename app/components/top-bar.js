cfdb07ae7e940bed46165f8911ff9e7c68ece95e
iamtimleonard <timleonardcello@gmail.com>
2021-06-23 17:37:01 -0400

    update markup and styling for top-bar

--- app/components/top-bar.js
+++ app/components/top-bar.js
@@ -15,6 +15,35 @@ export default Component.extend(CurrentUserMixin, ErrorHandlingMixin, {
   toggleRoleErrors: [],
   alert: service("sweet-alert"),
 
+  make: [
+    {text: "assignment", link: "assignments.new", restricted: true},
+    {text: "workspace", link: "workspaces.new", restricted: true, children: [ 
+      {link: "workspaces.new", text: "new"}, 
+      {link: "import", text: "import"}, 
+      {link: "workspaces.copy", text: "copy"}, 
+      {link: "vmt.import", text: "vmt"}
+    ]},
+    {text: "problem", link: "problem"}, 
+    {text: "class", link: "sections.new"},
+    {text: "users", link: "users.new"},
+  ],
+
+  do: [
+    {text: "solve a problem", link: "assignments", restricted: false},
+    {text: "review submitted work", link: "workspaces", restricted: false},
+    {text: "mentor submission", link: "responses", restricted: false},
+    {text: "manage classes", link: "sections", restricted: true},
+    {text: "manage users", link: "users", restricted: true},
+  ],
+
+  find: [
+    {text: "assignment", link: "assignments", restricted: false},
+    {text: "workspace", link: "workspaces", restricted: false},
+    {text: "problem", link: "problems", restricted: false},
+    {text: "class", link: "sections", restricted: false},
+    {text: "users", link: "users", restricted: true},
+  ],
+
   isStudent: computed("user.actingRole", "user.id", function () {
     return (
       this.user.get("isStudent") || this.user.get("actingRole") === "student"
