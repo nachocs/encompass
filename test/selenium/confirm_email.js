bf8f08929b64fc3edfc4b279d77f1d36d31ddb33
iamtimleonard <timleonardcello@gmail.com>
2021-07-08 11:51:18 -0400

    fix url for confirm_email test

--- test/selenium/confirm_email.js
+++ test/selenium/confirm_email.js
@@ -10,7 +10,7 @@ const css = require('./selectors');
 const fixtures = require('./fixtures/confirm_email');
 
 const host = helpers.host;
-const confirmPath = '/#/auth/confirm';
+const confirmPath = '/auth/confirm';
 
 const confirmLink = `${host}${confirmPath}/${fixtures.userLiveToken.token}`;
 const invalidResetLink = `${host}${confirmPath}/${fixtures.userLiveToken.invalidToken}`;
